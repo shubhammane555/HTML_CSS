cc9debae3a0d167e5e4680c6343f5d695a4d8cbb
shubhammane555 <mane02939@gmail.com>
2022-11-21 20:44:06 +0530

    html&css code commit

---
+++ html_css_part2/HTML/a8.java
@@ -0,0 +1,18 @@
+<html>
+<title>Regi </title>
+<body>
+<center><pre>
+<h1>student form </h1>
+<firm>
+enter fname:<input type ="text" name ="n1"><br>
+enter lname:<input type ="text" name ="n2"><br>
+enter mobno:<input type ="text" name ="n3"><br>
+enter Emilid:<input type ="text" name ="n4"><br>
+<hr size=2 color=red>
+<input type ="Submit" value ="Submit">
+<input type ="Reset" value ="Reset">
+</pre>
+</form>
+</center>
+</body>
+</html>
---
+++ html_css_part2/css/image.java
@@ -0,0 +1,11 @@
+<html>
+<head>
+<style>
+body{background-image:url('aaa.jpg');
+background-repeat:no-repeat;}
+</style>
+</head>
+<body>
+<h1>plz check background image</h1>
+</body>
+</html>
